ad721040d58b58ef84497882ec41b50d0df0e215
kinmax <kin.gusmao@acad.pucrs.br>
2016-06-06 02:07:58 -0300

    tictactoe

--- display.c
+++ display.c
@@ -3,32 +3,32 @@
 
 void	display_board(char board[3][3])
 {
-  int	i;
-  int	j;
+    int	i;
+    int	j;
 
-  for (i = 0; i < 3; i++)
+    for (i = 0; i < 3; i++)
     {
-      for (j = 0; j < 3; j++)
-	{
-          if (j > 0)
-            printf("%s|", BLUE);
-          if (board[i][j] >= 49)
-            printf("  %s%c%s  " , GREY, board[i][j], BLUE);
-          else if (board[i][j] == 0)
-            printf("  %sX%s  ", YELLOW, BLUE);
-          else if (board[i][j] == 1)
-            printf("  %sO%s  ", L_BLUE, BLUE);
-	}
-      if (i < 2)
-	printf("\n-----------------\n");
+        for (j = 0; j < 3; j++)
+        {
+            if (j > 0)
+                printf("%s|", BLUE);
+            if (board[i][j] >= 49)
+                printf("  %s%c%s  " , GREY, board[i][j], BLUE);
+            else if (board[i][j] == 0)
+                printf("  %sX%s  ", YELLOW, BLUE);
+            else if (board[i][j] == 1)
+                printf("  %sO%s  ", L_BLUE, BLUE);
+        }
+        if (i < 2)
+            printf("\n-----------------\n");
     }
-  printf("\n%s", WHITE);
+    printf("\n%s", WHITE);
 }
 
 void	display_error(int error)
 {
-  if (error == -1)
-    printf("Error: please enter a number between 1 and 9\n");
-  else if (error != -42)
-    printf("Error : the cell %d is already used, please choose another cell", error + 1);
+    if (error == -1)
+        printf("Error: please enter a number between 1 and 9\n");
+    else if (error != -42)
+        printf("Error : the cell %d is already used, please choose another cell", error + 1);
 }
